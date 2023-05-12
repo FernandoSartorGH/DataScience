e511a8716c6ab144b985b863f3a5c8d00f9af60d
Fernando dos Santos Sartor <fernandosartor@outlook.com>
2023-05-12 11:39:12 -0300

    update

---
+++ MachineLearning/Models/6 - PCA/my_PCA.py
@@ -0,0 +1,44 @@
+# STEPS
+
+# Subtract the mean from X
+# Calculate Cov(X,X)
+# Calculate eigenvectors and eigenvalues of the covariance matrix
+# Sort the eigen vectors acording the their eigen values 
+# Transform the original n-dimensional data into k dimension
+
+
+import numpy as np
+
+class PCA:
+
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.eigenvectors = None
+        
+    def fit(self, X):
+        
+        # mean centering
+        X = (X -  X.mean())/X.std()
+
+        # covariance, functions needs samples as columns
+        cov = np.cov(X.T, bias=1)
+
+        # eigenvectors, eigenvalues
+        eigenvalues, eigenvectors = np.linalg.eig(cov)
+
+        # sort by eigenvalues
+        idxs = np.argsort(eigenvalues)[::-1]
+        eigenvalues = eigenvalues[idxs]
+        eigenvectors = eigenvectors[idxs]
+
+        self.components = eigenvectors
+
+    def transform(self, X):
+        
+        # mean centering
+        X = (X -  X.mean())/X.std()
+
+        pca = np.dot(np.array(X), self.components)
+        pca  = pca[:,:self.n_components]
+        
+        return pca
